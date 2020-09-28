e49f89bc75d8e5850f935f3e93d1bc235bad82c8
rickypazd <35882906+rickypazd@users.noreply.github.com>
2020-09-28 18:21:35 -0400

    as

--- server/motonet/src/Viaje/ViajeHilo.java
+++ server/motonet/src/Viaje/ViajeHilo.java
@@ -15,7 +15,7 @@ public class ViajeHilo {
                 objSend.put("type", "viajeEntrante");
                 objSend.put("data", obj);
                 objSend.put("estado", "exito");
-                SocketServer.sendUser(objSend.toString(), "70597ea4-fa68-4792-8a5f-39225c2a36a8");
+                SocketServer.sendUser(objSend.toString(), "1eab302f-5e84-41b2-bfca-7852eb52fc4a");
             }
         };
         thread.start();
