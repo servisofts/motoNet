bc9238227d2fb1099f8b5ae2e0a751f637cff96c
juliohurtado123 <juliocesar@gmail.com>
2020-09-30 10:38:56 -0400

    areglo

--- cliente-nuevo-react-native/motonetcliente/src/component/Carga/index.js
+++ cliente-nuevo-react-native/motonetcliente/src/component/Carga/index.js
@@ -1,45 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 
 
-const Carga = (props) => {
-    const [obj, setObj] = React.useState(false);
+class Carga extends Component {
 
-    if (obj) {
-        if (props.state.usuarioReducer.usuarioLog) {
+  constructor(props) {
+    super(props);
+    this.state = {
+        obj:false
+    };
+  }
+
+  componentDidMount() { // B
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+    const yourFunction = async () => {
+        await delay(3000);
+        if (this.props.state.usuarioReducer.usuarioLog) {
             //si tengo un usuario
-            if (!props.state.viajesReducer.viaje) {
-                props.state.navigationReducer.replace("InicioPage");
+            if (!this.props.state.viajesReducer.viaje) {
+                this.props.state.navigationReducer.replace("InicioPage");
             }
-            props.state.navigationReducer.replace("ViajeEsperaPage");
+            this.props.state.navigationReducer.replace("ViajeEsperaPage");
             //falta comprovar si tengo viaje 
             // si no tengo viaje voy a incio
             //si tengo viaje voy a la etapa que corresponda del viaje.
         }
-        props.state.usuarioReducer.estado = ""
-        props.state.navigationReducer.replace("LoginPage");
-        //setObj(false);
-        return <View />
-    } else {
-        const delay = ms => new Promise(res => setTimeout(res, ms));
-        const yourFunction = async () => {
-            await delay(3000);
-            console.log("Waited 5s");
-            setObj(true);
-            return <View />;
-        };
-        yourFunction();
-    }
-
+        this.props.state.usuarioReducer.estado = ""
+        this.props.state.navigationReducer.replace("LoginPage");
+        return <View />;
+    };
+    yourFunction();
+  }
+  render(){
     return (
-
         <View >
-
-     
-
         </View>
     );
+}
 }
 
 const initStates = (state) => {
