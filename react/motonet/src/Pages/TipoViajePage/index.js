cfe8ae51f3d0bdc5168a46fa160476207d9f379c
Javier W. Flores <willams_21@hotmail.com>
2020-10-22 13:19:09 -0400

    a

--- react/motonet/src/Pages/TipoViajePage/index.js
+++ react/motonet/src/Pages/TipoViajePage/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import NaviDrawer from '../../Components/NaviDrawer';
-import { CircularProgress, Grid } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
