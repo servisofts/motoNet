ca048c498e54b7a9a5fab2f7fa9e132e13693a56
rickypazd <35882906+rickypazd@users.noreply.github.com>
2020-10-26 14:58:33 -0400

    sa

--- react-native/motoNet/android/app/src/main/java/com/motonet/MainActivity.java
+++ react-native/motoNet/android/app/src/main/java/com/motonet/MainActivity.java
@@ -15,7 +15,11 @@ import com.facebook.react.ReactActivity;
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * Returns the
+   *
+   *
+   *
+   * name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
   @Override
--- server/motonet/src/App.java
+++ server/motonet/src/App.java
@@ -7,7 +7,7 @@ import util.console;
 public class App {
     public static void main(String[] args) throws Exception {
         console.error("Iniciando server servisofts");
-        if (!Config.validate()) {`
+        if (!Config.validate()) {
             console.error("Server closed.");
             return;
         }
