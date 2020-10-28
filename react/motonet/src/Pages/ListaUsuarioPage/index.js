0580b550f8d651cac31210473d4e58e31beeae0f
Javier W. Flores <willams_21@hotmail.com>
2020-10-28 17:16:03 -0400

    a

--- react/motonet/src/Pages/ListaUsuarioPage/index.js
+++ react/motonet/src/Pages/ListaUsuarioPage/index.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import NaviDrawer from '../../Components/NaviDrawer';
-import TableDetail from '../../Components/Table';
 import TableNewMe from '../../Components/TableNewMe';
 import { CircularProgress, Grid } from '@material-ui/core';
 import { Link } from 'react-router-dom';
