4be11d71c4ed10a4391393e35de1cfcd1c0798e8
Edson Gonzalo Mamani Veizaga <edson.nur@gmail.com>
2020-10-31 15:19:29 -0400

    add

--- cliente-nuevo-react-native/motonetcliente/android/app/src/main/java/com/motonetcliente/MainApplication.java
+++ cliente-nuevo-react-native/motonetcliente/android/app/src/main/java/com/motonetcliente/MainApplication.java
@@ -15,7 +15,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
-import com.reactnativecommunity.geolocation.GeolocationPackage;
+//import com.reactnativecommunity.geolocation.GeolocationPackage;
 import java.util.List;
 //----IMPORT FACEBOOK----
 
@@ -37,7 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-           packages.add(new GeolocationPackage());
+          // packages.add(new GeolocationPackage());
           return packages;
         }
 
