a81334953cc54218f449bb4d42611279479d1780
Edson Gonzalo Mamani Veizaga <edson.nur@gmail.com>
2021-06-07 11:21:46 -0400

    add

--- cliente-nuevo-react-native/motonetcliente/index.js
+++
@@ -1,6 +0,0 @@
-import {AppRegistry,LogBox} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-console.disableYellowBox = true;
-LogBox.ignoreLogs(['Warning:','Animated:','VirtualizedList:','VirtualizedLists'])
-AppRegistry.registerComponent(appName, () => App);
